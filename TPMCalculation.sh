fb56e5568139fa205981ac337bd3daadfa96c849
luzengAdelaide <sophiezenglu@126.com>
2018-03-18 19:22:08 +1030

    change directory path

--- TPMCalculation.sh
+++ TPMCalculation.sh
@@ -2,11 +2,8 @@
 
 # Calulate the TPM value for each RNA-Seq data
 # Run jobs on slurm matchine
-# Invoked by:
-#
-# READPATH=/data/rc003/lu/transcriptome/$i sbatch TPMCalculation.sh
-#
-# $i=genome (e.g. human)
+
+# $1=genome (e.g. human)
 
 #SBATCH -p batch
 #SBATCH -N 1
@@ -34,35 +31,42 @@ FILES2=($(ls $READPATH | rev | cut -c 15- | rev | uniq))
 FILES3=($(ls $READPATH | rev | cut -c 17- | rev | uniq))
 
 # Download RNA-Seq data
+cd /data/rc003/lu/transcriptome/$1/
 wget ftp://ftp-trace.ncbi.nlm.nih.gov/sra/sra-instant/reads/ByRun/sra/SRR/SRRxxx/SRRxxxxx/*.sra
 
 # Transfer sra format to fastq format
 for i in *.sra
 do
-fastq-dump.2.7.0 -I --split-files $i
+fastq-dump.2.7.0 -I --split-files $1/$i
 done
 
 # Check the quality of RNA-Seq data
+mkdir qc
 for i in *.fastq
 do
     echo $i
-    fastqc -o qc/ $i
+    fastqc -o $1/qc/ $1/$i
 done
 
 # Adapter trimming the fastq files 
 # and then check the RNA-Seq data quality 
-trim_galore --stringency 6 -o $i/trim/ --paired -fastqc_args "-t 8" ${READPATH}/$i/${FILES3[$SLURM_ARRAY_TASK_ID]}_1.fastq.gz ${READPATH}/$i/${FILES3[$SLURM_ARRAY_TASK_ID]}_2.fastq.gz
+mkdir /data/rc003/lu/transcriptome/$1/trim
+trim_galore --stringency 6 -o /data/rc003/lu/transcriptome/$1/trim/ --paired -fastqc_args "-t 8" /data/rc003/lu/transcriptome/$1/${FILES3[$SLURM_ARRAY_TASK_ID]}_1.fastq.gz /data/rc003/lu/transcriptome/$1/${FILES3[$SLURM_ARRAY_TASK_ID]}_2.fastq.gz
 # strigency: overlap with adapter sequence required to trim a sequences, default is '1', the lower the number
 # the lower strigency
 
 # For single-end RNA-Seq data
-trim_galore --clip_R1 5 --three_prime_clip_R1 5 -o $i/trim/ -fastqc_args "-t 8" ${READPATH}/${FILES2[$SLURM_ARRAY_TASK_ID]}_1.fastq.gz
+mkdir /data/rc003/lu/transcriptome/$1/trim
+trim_galore --clip_R1 5 --three_prime_clip_R1 5 -o $/data/rc003/lu/transcriptome/$1/trim/ -fastqc_args "-t 8" /data/rc003/lu/transcriptome/$1/${FILES2[$SLURM_ARRAY_TASK_ID]}_1.fastq.gz
+
+# Make directory to store star output
+mkdir /data/rc003/lu/transcriptome/$1/RSEM/
 
 # Then prepare rsem reference
-~/RSEM-1.3.0/rsem-prepare-reference --gtf $i.gtf --bowtie2 --bowtie2-path /home/lu/bowtie2-2.2.5 ../genomes/$i.fa $i_index/$iGenome
+~/RSEM-1.3.0/rsem-prepare-reference --gtf /data/rc003/lu/transcriptome/gtf_files/$1.gtf --bowtie2 --bowtie2-path /apps/software/Bowtie2/2.2.6-foss-2015b/bin/ /data/rc003/lu/transcriptome/genomes/$1.fa /data/rc003/lu/transcriptome/$1/$1_index/$1Genome
 
 # Then calculate the gene expression (TPM), including pair-end and single-end data
 # Pair-end
-~/RSEM-1.3.0/rsem-calculate-expression -p 8 --bowtie2 --paired-end ${READPATH}/$i/trim/${FILES3[$SLURM_ARRAY_TASK_ID]}_1_val_1.fq.gz ${READPATH}/$i/trim/${FILES3[$SLURM_ARRAY_TASK_ID]}_2_val_2.fq.gz $i_index/$iGenome ${READPATH}/$i/trim/${FILES3[$SLURM_ARRAY_TASK_ID]}
+~/RSEM-1.3.0/rsem-calculate-expression -p 8 --bowtie2 --paired-end /data/rc003/lu/transcriptome/$1/trim/${FILES3[$SLURM_ARRAY_TASK_ID]}_1_val_1.fq.gz /data/rc003/lu/transcriptome/$1/trim/${FILES3[$SLURM_ARRAY_TASK_ID]}_2_val_2.fq.gz /data/rc003/lu/transcriptome/$1/$1_index/$1Genome /data/rc003/lu/transcriptome/$1/RSEM/${FILES3[$SLURM_ARRAY_TASK_ID]}
 # Single-end
-~/RSEM-1.3.0/rsem-calculate-expression -p 8 --bowtie2 ${READPATH}/$i/trim/${FILES2[$SLURM_ARRAY_TASK_ID]}_1_trimmed.fq.gz $i_index/$iGenome ${READPATH}/$i/trim/${FILES2[$SLURM_ARRAY_TASK_ID]}
+~/RSEM-1.3.0/rsem-calculate-expression -p 8 --bowtie2 /data/rc003/lu/transcriptome/$1/trim/${FILES2[$SLURM_ARRAY_TASK_ID]}_1_trimmed.fq.gz /data/rc003/lu/transcriptome/$1/$1_index/$1Genome /data/rc003/lu/transcriptome/$1/RSEM/${FILES2[$SLURM_ARRAY_TASK_ID]}
